de5101db25c9985384717e1fa5c7ab91377040e4
Joe <ckdfo1995@naver.com>
2019-04-06 23:05:13 +0900

    aa

--- app/src/main/java/com/example/parkingsystem/MainActivity.java
+++ app/src/main/java/com/example/parkingsystem/MainActivity.java
@@ -22,7 +22,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-import static com.kakao.util.helper.Utility.getPackageInfo;
+//import static com.kakao.util.helper.Utility.getPackageInfo;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
@@ -72,23 +72,23 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DB_test.class);
         startActivity(intent);
     }
-
-    public static String getKeyHash(final Context context) {
-        PackageInfo packageInfo = getPackageInfo(context, PackageManager.GET_SIGNATURES);
-        if (packageInfo == null)
-            return null;
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
-            } catch (NoSuchAlgorithmException e) {
-                Log.w("main", "Unable to get MessageDigest. signature=" + signature, e);
-            }
-        }
-        return null;
-    }
+//
+//    public static String getKeyHash(final Context context) {
+//        PackageInfo packageInfo = getPackageInfo(context, PackageManager.GET_SIGNATURES);
+//        if (packageInfo == null)
+//            return null;
+//
+//        for (Signature signature : packageInfo.signatures) {
+//            try {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+//            } catch (NoSuchAlgorithmException e) {
+//                Log.w("main", "Unable to get MessageDigest. signature=" + signature, e);
+//            }
+//        }
+//        return null;
+//    }
 
 
 }
